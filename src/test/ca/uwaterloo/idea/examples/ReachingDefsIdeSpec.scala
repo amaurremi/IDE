2cb3181c4ff8f0263d8a4a10665ca1a8e868342d
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-06-01 23:33:27 -0400

    refactor

--- src/test/ca/uwaterloo/idea/examples/ReachingDefsIdeSpec.scala
+++ src/test/ca/uwaterloo/idea/examples/ReachingDefsIdeSpec.scala
@@ -5,7 +5,7 @@ import ca.uwaterloo.ide.solver.IdeSolver
 import com.ibm.wala.core.tests.callGraph.CallGraphTestUtil
 import com.ibm.wala.core.tests.util.TestConstants
 import com.ibm.wala.dataflow.IFDS.TabulationProblem
-import com.ibm.wala.examples.analysis.dataflow.DataflowTest
+import com.ibm.wala.examples.analysis.dataflow.{ContextSensitiveReachingDefs, DataflowTest}
 import com.ibm.wala.ipa.callgraph._
 import com.ibm.wala.ipa.callgraph.impl.Util
 import com.ibm.wala.ipa.cfg.BasicBlockInContext
@@ -17,20 +17,22 @@ import com.ibm.wala.util.config.AnalysisScopeReader
 object ReachingDefsIdeSpec {
 
   def main(args: Array[String]): Unit = {
-    val problem = new ReachingDefsIdeProblem
+    val problem = new ReachingDefsIdeProblem(cg)
     val result = problem.solvedResult
     print("yay")
   }
+
+  private[this] val cg = {
+    val scope = AnalysisScopeReader.readJavaScope(TestConstants.WALA_TESTDATA, null, classOf[DataflowTest].getClassLoader)
+    val cha = ClassHierarchy.make(scope)
+    val entrypoints = com.ibm.wala.ipa.callgraph.impl.Util.makeMainEntrypoints(scope, cha, "Ldataflow/StaticDataflow")
+    val options: AnalysisOptions = CallGraphTestUtil.makeAnalysisOptions(scope, entrypoints)
+    val builder: CallGraphBuilder = Util.makeZeroOneCFABuilder(options, new AnalysisCache, cha, scope)
+    builder.makeCallGraph(options, null)
+  }
 }
 
-class ReachingDefsIdeProblem extends IdeFromIfdsBuilder with IdeSolver {
-
-  private[this] val scope = AnalysisScopeReader.readJavaScope(TestConstants.WALA_TESTDATA, null, classOf[DataflowTest].getClassLoader)
-  private[this] val cha = ClassHierarchy.make(scope)
-  private[this] val entrypoints = com.ibm.wala.ipa.callgraph.impl.Util.makeMainEntrypoints(scope, cha, "Ldataflow/StaticDataflow")
-  private[this] val options: AnalysisOptions = CallGraphTestUtil.makeAnalysisOptions(scope, entrypoints)
-  private[this] val builder: CallGraphBuilder = Util.makeZeroOneCFABuilder(options, new AnalysisCache, cha, scope)
-  private[this] val cg: CallGraph = builder.makeCallGraph(options, null)
+class ReachingDefsIdeProblem(cg: CallGraph) extends IdeFromIfdsBuilder with IdeSolver {
 
   override type F = Pair[CGNode, Integer]
   override type Node = BasicBlockInContext[IExplodedBasicBlock]
