4e0f9a37955d7e8d3cfa4c665c0251d66cc64684
Marianna Rapoport <mrapoport@uwaterloo.ca>
2015-06-01 23:20:52 -0400

    fixing error

--- src/test/ca/uwaterloo/idea/examples/ReachingDefsIdeProblem.scala
+++ src/test/ca/uwaterloo/idea/examples/ReachingDefsIdeProblem.scala
@@ -4,13 +4,14 @@ import ca.uwaterloo.ide.conversion.IdeFromIfdsBuilder
 import ca.uwaterloo.ide.solver.IdeSolver
 import com.ibm.wala.core.tests.callGraph.CallGraphTestUtil
 import com.ibm.wala.core.tests.util.TestConstants
-import com.ibm.wala.dataflow.IFDS.PartiallyBalancedTabulationProblem
+import com.ibm.wala.dataflow.IFDS.TabulationProblem
 import com.ibm.wala.examples.analysis.dataflow.DataflowTest
 import com.ibm.wala.ipa.callgraph._
 import com.ibm.wala.ipa.callgraph.impl.Util
 import com.ibm.wala.ipa.cfg.BasicBlockInContext
 import com.ibm.wala.ipa.cha.ClassHierarchy
 import com.ibm.wala.ssa.analysis.IExplodedBasicBlock
+import com.ibm.wala.util.collections.Pair
 import com.ibm.wala.util.config.AnalysisScopeReader
 
 object ReachingDefsIdeSpec {
@@ -31,9 +32,11 @@ class ReachingDefsIdeProblem extends IdeFromIfdsBuilder with IdeSolver {
   private[this] val builder: CallGraphBuilder = Util.makeZeroOneCFABuilder(options, new AnalysisCache, cha, scope)
   private[this] val cg: CallGraph = builder.makeCallGraph(options, null)
 
-  override type F = (CGNode, Integer)
-  override val walaIfdsProblem: PartiallyBalancedTabulationProblem[BasicBlockInContext[IExplodedBasicBlock], CGNode, (CGNode, Integer)] = new ReachingDefsProblem(cg, new AnalysisCache)
+  override type F = Pair[CGNode, Integer]
   override type Node = BasicBlockInContext[IExplodedBasicBlock]
   override type Procedure = CGNode
+  override val walaIfdsProblem
+    : TabulationProblem[Node, Procedure, F]
+    = new ReachingDefsProblem(cg, new AnalysisCache)
 }
 
